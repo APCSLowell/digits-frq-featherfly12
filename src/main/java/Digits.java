d709e34aafea055c71c4c90d0716585ffa1c98c6
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 16:34:32 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 
-public class Digits
-{
+public class Digits {
 
-	private ArrayList<Integer> digitList;
+  private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    digitList = new ArrayList <Integer>();
-		if(num==0)
-			digitList.add(0);
-		int n = num;
-		while(n>0){
-			digitList.add(0,n%10);
-			n=n/10;
-		}
-	}
+  public Digits(int num) {
+    digitList = new ArrayList<Integer>();
+    if (num == 0) {
+      digitList.add(0);
+    } else {
+      int n = num;
+      while (n > 0) {
+        digitList.add(0, n % 10);
+        n = n / 10;
+      }
+    }
+  }
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		for(int i = 0;i<digitList.size()-1;i++){
-			if(digitList.get(i)>=digitLIst.get(i+1))
-				return false;
-		}
-		return true;
+  public boolean isStrictlyIncreasing() {
+    for (int i = 0; i < digitList.size() - 1; i++) {
+      if (digitList.get(i) >= digitList.get(i + 1)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	}
-	
-	public String toString()
-	{
-		return digitList.toString();
-	}
+  public String toString() {
+    return digitList.toString();
+  }
 }
